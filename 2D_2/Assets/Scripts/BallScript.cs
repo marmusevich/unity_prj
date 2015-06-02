76ac5ea0b6f5e2da5db5a8b5d18fbf8d6cd6e17e
marmusevich <myrmik@gmail.com>
2015-06-02 13:00:30 +0300

    -

---
+++ 2D_2/Assets/Scripts/BallScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallScrypt : MonoBehaviour
+{
+    private bool ballIsActive;
+    private Vector3 ballPosition;
+    public Vector2 ballInitialForce = new Vector2(100.0f, 300.0f);
+    public GameObject playerObject;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        ballIsActive = false;
+        ballPosition = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") == true)
+        {
+            if (!ballIsActive)
+            {
+                GetComponent<Rigidbody2D>().AddForce(ballInitialForce);
+                ballIsActive = true;
+            }
+            if (!ballIsActive && playerObject != null)
+            {
+                ballPosition.x = playerObject.transform.position.x;
+                gameObject.transform.position = ballPosition;
+            }
+        }
+    }
+}
---
+++ 2D_2/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour 
+{
+    public float playerVelosity;
+    public float boundary;
+    private Vector3 playerPosition;
+
+	// Use this for initialization
+	void Start () 
+    {
+        playerPosition = gameObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        playerPosition.x += Input.GetAxis("Horizontal") * playerVelosity;
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        if (playerPosition.x < -boundary)
+        {
+            playerPosition.x = -boundary;
+        }
+        if (playerPosition.x > boundary)
+        {
+            playerPosition.x = boundary;
+        }
+        
+        gameObject.transform.position = playerPosition;
+	}
+}
