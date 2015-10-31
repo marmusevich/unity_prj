5466a76e5a9326d70fb8bcb64dda00786b376e7f
marmusevich <myrmik@gmail.com>
2015-10-31 10:28:41 +0200

    +

---
+++ test2d_01/Assets/old/2d_2_Assets/Scripts/BlockScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class BlockScript : MonoBehaviour
+{
+	public int hitsToKill;
+	public int points;
+	private int numberOfHits;
+	private GameObject player;
+
+	// Use this for initialization
+	void Start ()
+	{
+		numberOfHits = 0;
+		player = GameObject.Find ("player");
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+
+	void OnCollisionEnter2D (Collision2D collision)
+	{
+		if (collision.gameObject.name == "ball") {
+			numberOfHits++;
+
+			if (numberOfHits >= hitsToKill) {
+				if (player != null) {
+					player.SendMessage ("AddPoints", points);
+				}
+				Destroy (this.gameObject);
+			}
+		}
+	}
+}
+
