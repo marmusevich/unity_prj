c0c3deceedf20b48a36332140b39dabe9ccc85cc
marmusevich <myrmik@gmail.com>
2015-11-01 11:17:01 +0200

    +

--- test2d_01/Assets/old/TestTreeDevelopment/TreeDevelopment.cs
+++ test2d_01/Assets/old/TestTreeDevelopment/TreeDevelopment.cs
@@ -1,15 +1,18 @@
-﻿using System;
+﻿// дерево развития в игре, прототип
+
+
+using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Serialization;
 
 // добавить проверки
 
 
+
+/* собственно классс
 namespace TestTreeDevelopment
 {
     public class TreeDevelopment
@@ -184,3 +187,61 @@ namespace TestTreeDevelopment
         }
     }
 }
+*/
+
+
+/* -- test for this class
+namespace TestTreeDevelopment
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string fn = "m:/_GitHub/c#/TestTreeDevelopment/TestTreeDevelopment/TreeDevelopment.xml";
+			string fn2 = "m:/_GitHub/c#/TestTreeDevelopment/TestTreeDevelopment/TreeDevelopment2.xml";
+			
+			
+			TreeDevelopment td = new TreeDevelopment();
+			// Init
+			td.Add("Base1", 0);
+			td.Add("Base2", 0);
+			td.Add("der1", 100);
+			td.Add("der2", 150);
+			td.Add("der3", 170);
+			td.Add("der4", 180);
+			
+			//SetDepend
+			td.SetDepend("Base1",   "der1");
+			td.SetDepend("der1",    "der2");
+			td.SetDepend("Base2",   "der2");
+			td.SetDepend("der2",    "der3");
+			td.SetDepend("der2",    "der4");
+			
+			//td.Print();
+			
+			//System.Console.WriteLine("\nSetStudie ->  Base1, Base2" );
+			//td.SetStudie("Base1");
+			//td.SetStudie("Base2");
+			////td.Print();
+			
+			//System.Console.WriteLine("\nSetStudie ->  der1" );
+			//td.SetStudie("der1");
+			////td.Print();
+			
+			
+			td.Save(fn);
+			
+			
+			System.Console.WriteLine("\n\n\n -----  TD1  ------");
+			TreeDevelopment td1 = new TreeDevelopment();
+			td1.Load(fn);
+			td1.Print();
+			
+			td1.Save(fn2);
+			
+			
+			System.Console.ReadLine();
+		}
+	}
+}
+*/
