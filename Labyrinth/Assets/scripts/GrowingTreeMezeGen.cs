3bfd922ac3cf294490aaf45a341d1dfc6692eccb
lexa <myrmik@gmail.com>
2016-05-10 09:53:25 +0300

    add prj Labyrinth

---
+++ Labyrinth/Assets/scripts/GrowingTreeMezeGen.cs
@@ -0,0 +1,154 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+    using MazePoint = IntVector2D;
+    using Direction = IntVector2D;
+
+
+    public class GrowingTreeMezeGen : BaseMazeGenerator
+    {
+        bool[,] maze;
+        int xCount;
+        int yCount;
+
+        //процент извилистости
+        float windingPrecent;
+
+        #region private member
+
+        /// <summary>
+        /// вернуть случайную точьку с учетом границ, границы не поподают
+        /// </summary>
+        /// <returns>The random position.</returns>
+        private MazePoint getRandomPos()
+        {
+            return new MazePoint(Random.Range(1, xCount), Random.Range(1, yCount));
+        }
+
+        /// <summary>
+        /// сделать проход в позиции (<c>pos</c>)
+        /// </summary>
+        /// <param name="pos">Position.</param>
+        /// <param name="val">значение по умолчанию проход (<c>false</c>)</param>
+        private void Carve(MazePoint pos, bool val = false)
+        {
+            if(CheckPosInMaze(pos))
+                maze[pos.x, pos.y] = val;
+        }
+
+        /// <summary>
+        /// вернуть значение в точьке
+        /// </summary>
+        /// <returns>value</returns>
+        /// <param name="pos">Position.</param>
+        private bool GetVal(MazePoint pos)
+        {
+            if(CheckPosInMaze(pos))
+                return maze[pos.x, pos.y];
+            return false;
+        }
+
+        /// <summary>
+        /// проверка на возможность сделать проход в точке (<c>pos</c>) по направлению (<c>dir</c>)
+        /// </summary>
+        /// <returns><c>true</c>, проход может быть сделан</returns>
+        /// <param name="pos">Position.</param>
+        /// <param name="dir">Direction.</param>
+        private bool CheckCanCarve(MazePoint pos, MazePoint dir)
+        {
+            //если через две клетки выход за границы или последущее за границами то не проверяем саму точьку
+            if(CheckPosInMaze(pos + 3 * dir) && CheckPosInMaze(pos + 2 * dir))
+                return GetVal(pos + 2 * dir);
+            return false;
+        }
+
+        /// <summary>
+        /// проверка точьки (<c>pos</c>) на вхождение внутрь границ массива
+        /// </summary>
+        /// <returns><c>true</c>,если точька внутри границ масива</returns>
+        /// <param name="pos">Position.</param>
+        private bool CheckPosInMaze(MazePoint pos)
+        {
+            return pos.x >= 0 && pos.x < xCount && pos.y >= 0 && pos.y < yCount;
+        }
+
+        /// <summary>
+        /// вернуть разрешонные направления
+        /// </summary>
+        /// <returns>The allowed dir.</returns>
+        /// <param name="pos">Position.</param>
+        private List<Direction> ReturnAllowedDir(MazePoint pos)
+        {
+            List<Direction> ret = new List<Direction>();
+            if(CheckCanCarve(pos, Direction.Down))
+                ret.Add(Direction.Down);
+            if(CheckCanCarve(pos, Direction.Up))
+                ret.Add(Direction.Up);
+            if(CheckCanCarve(pos, Direction.Left))
+                ret.Add(Direction.Left);
+            if(CheckCanCarve(pos, Direction.Right))
+                ret.Add(Direction.Right);
+
+            return ret;
+        }
+
+        #endregion
+
+        public GrowingTreeMezeGen(float _windingPrecent = 0.5f)
+        {
+            windingPrecent = _windingPrecent;
+        }
+
+
+        public override void Generate(ref bool[,] _maze)
+        {
+            maze = _maze;
+            xCount = maze.GetLength(0);
+            yCount = maze.GetLength(1);
+
+            //нет генерации если слишком мал лабиринт
+            if(xCount == 1 || yCount == 1)
+                return;
+
+            FullWall(ref maze);
+
+            Direction lastDir = Direction.Zero;
+            Direction dir = Direction.Zero;
+            Stack<MazePoint> cells = new Stack<MazePoint>();
+
+
+
+            MazePoint point = getRandomPos(); //MazePoint.One;
+            cells.Push(point);
+            Carve(point);
+
+            while (cells.Count > 0)
+            {
+                point = cells.Peek();
+                List<Direction> dirs = ReturnAllowedDir(point);
+                if(dirs.Count > 0)
+                {
+                    if(dirs.Contains(lastDir) && ( Random.value > windingPrecent ))
+                        dir = lastDir;
+                    else
+                        dir = dirs[Random.Range(0, dirs.Count)];
+
+                    Carve(point + dir);
+                    Carve(point + 2 * dir);
+
+                    cells.Push(point + 2 * dir);
+                    lastDir = dir;
+                } else
+                {
+                    cells.Pop();
+                    lastDir = Direction.Zero;
+                }
+            }
+        }
+    }
+//  Debug.Log(string.Format("{0} -> {1} = {2}", numer, tmp, Sprites[tmp].name));
+
+
+
+
---
+++ Labyrinth/Assets/scripts/MyUtilsFunction.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyUtilsFunction
+{
+    /// <summary>
+    /// вернуть размеры в еденицах unity например префаба
+    /// </summary>
+    /// <returns>The game object size.</returns>
+    /// <param name="obj">Object.</param>
+    public static Vector2 GetGameObjectSize(GameObject obj)
+    {
+        float minX = float.MaxValue;
+        float maxX = float.MinValue;
+        float minY = float.MaxValue;
+        float maxY = float.MinValue;
+        SpriteRenderer[] sprites = obj.GetComponentsInChildren<SpriteRenderer>();
+        foreach( SpriteRenderer sr in sprites )
+        {
+            float tmpMinX = sr.transform.position.x + sr.sprite.bounds.center.x - sr.sprite.bounds.size.x * sr.transform.localScale.x / 2f;
+            float tmpMaxX = sr.transform.position.x + sr.sprite.bounds.center.x + sr.sprite.bounds.size.x * sr.transform.localScale.x / 2f;
+            //if (tmpMinX < minX)
+            //{
+            //    minX = tmpMinX;
+            //}
+            //if (tmpMaxX > maxX)
+            //{
+            //    maxX = tmpMaxX;
+            //}
+            minX = Mathf.Min(tmpMinX, minX);
+            maxX = Mathf.Max(tmpMaxX, maxX);
+
+            float tmpMinY = sr.transform.position.x + sr.sprite.bounds.center.x - sr.sprite.bounds.size.x * sr.transform.localScale.x / 2f;
+            float tmpMaxY = sr.transform.position.x + sr.sprite.bounds.center.x + sr.sprite.bounds.size.x * sr.transform.localScale.x / 2f;
+            minY = Mathf.Min(tmpMinY, minY);
+            maxY = Mathf.Max(tmpMaxY, maxY);
+        }
+        return new Vector2(maxX - minX, maxY - minY);
+    }
+
+    /// <summary>
+    /// вернуть размер видимой области
+    /// </summary>
+    /// <param name="camera">Camera.</param>
+    /// <param name="topLeft">Top left.</param>
+    /// <param name="bottomRight">Bottom right.</param>
+    public static void ReturnScreenSizeInCamera(Camera camera, out Vector2 topLeft, out Vector2 bottomRight)
+    {
+        float width = camera.pixelWidth;
+        float height = camera.pixelHeight;
+        topLeft = camera.ScreenToWorldPoint(new Vector2(0, height));
+        bottomRight = camera.ScreenToWorldPoint(new Vector2(width, 0));
+        //Vector2 topRight = camera.ScreenToWorldPoint(new Vector2(width, height));
+        //Vector2 bottomLeft = camera.ScreenToWorldPoint(new Vector2(0, 0));
+    }
+
+
+
+
+    /* - вынести в общие класы
+        //Перемешивание списка
+        void Shuffle(int[] deck)
+        {
+            for(int i = 0; i < deck.Length; i++)
+            {
+                int temp = deck[i];
+                int randomIndex = Random.Range(0, deck.Length);
+                deck[i] = deck[randomIndex];
+                deck[randomIndex] = temp;
+            }
+        }
+
+
+        //Выбор элементов из набора без повторений
+        Transform[] spawnPoints;
+
+        Transform[] ChooseSet(int numRequired)
+        {
+            Transform[] result = new Transform[numRequired];
+            int numToChoose = numRequired;
+            for(int numLeft = spawnPoints.Length; numLeft > 0; numLeft--)
+            {
+                float prob = (float)numToChoose / (float)numLeft;
+                if(Random.value <= prob)
+                {
+                    numToChoose--;
+                    result[numToChoose] = spawnPoints[numLeft - 1];
+                    if(numToChoose == 0)
+                    {
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+        */
+
+
+
+}
