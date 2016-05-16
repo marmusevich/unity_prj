b62d4ffc8152bd6c582927c4c5934719fc600d59
marmusevich <myrmik@gmail.com>
2016-05-16 12:15:28 +0300

    add main menu

---
+++ Labyrinth/Assets/scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+
+	public void StartGameButtonPreset() 
+	{
+		SceneManager.LoadScene( "maze" );
+	}
+
+	public void SettingButtonPreset() 
+	{
+		Debug.Log( "SettingButtonPreset()");
+
+	}
+
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
