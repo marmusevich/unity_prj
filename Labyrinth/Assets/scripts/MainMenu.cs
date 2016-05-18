af9ee645a5f0432d835db07faf6aa4d354ab6c65
lexa <myrmik@gmail.com>
2016-05-18 13:57:39 +0300

    music

--- Labyrinth/Assets/scripts/MainMenu.cs
+++ Labyrinth/Assets/scripts/MainMenu.cs
@@ -2,29 +2,54 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour {
+public class MainMenu : MonoBehaviour
+{
+
+	public Animator startButton;
+	public Animator dlg_setting;
 
 
-	public void StartGameButtonPreset() 
+
+	public void StartGameButtonPreset()
 	{
 		SceneManager.LoadScene( "maze" );
 	}
 
-	public void SettingButtonPreset() 
-	{
-		Debug.Log( "SettingButtonPreset()");
 
+
+	public void OpenSettings()
+	{
+		startButton.SetBool( "isHidden", true );
+		dlg_setting.SetBool( "isHidden", false );
+		dlg_setting.enabled = true;
+	}
+
+	public void CloseSettings()
+	{
+		startButton.SetBool( "isHidden", false );
+		dlg_setting.SetBool( "isHidden", true );
+	}
+
+	public void ToggleMenu()
+	{
+		//contentPanel.enabled = true;
+
+		//bool isHidden = contentPanel.GetBool( "isHidden" );
+//		contentPanel.SetBool("isHidden", !isHidden);
+//		gearImage.enabled = true;
+//		gearImage.SetBool("isHidden", !isHidden);
 	}
 
 
+
 	// Use this for initialization
-	void Start () 
+	void Start()
 	{
 	
 	}
 	
 	// Update is called once per frame
-	void Update () 
+	void Update()
 	{
 	
 	}
